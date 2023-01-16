bf24c77146f546eaaf26e0687a5a7e01b8c082a1
Andrew <ajsantos050@gmail.com>
2023-01-16 09:59:14 -0500

    Starting a Netflix clone 2.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hey</h1>
+      <FontAwesomeIcon icon="bars"/>
+    </div>
+  );
+}
+
+export default App;
