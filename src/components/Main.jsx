2b5f63b04a44071daa3463e02c942953468042c6
Andrew <ajsantos050@gmail.com>
2023-01-16 21:28:16 -0500

    Adding main movie

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import request from '../Request'
+
 
 export default function Main() {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([])
+
+  const movie = movies[Math.floor(Math.random() * movies.length)]
+
+  useEffect(() => {
+    axios.get(request.requestPopular).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [])
+  console.log(movie)
+
   return (
-    <div>Main</div>
+    <section className='main__page'>
+      <div className='main__movie--wrapper'>
+        {/* <div className='movie__overlay'></div> */}
+        <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} className='main__movie--img' />
+      </div>
+    </section>
   )
 }
